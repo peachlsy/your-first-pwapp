ece1f88a58470d129af24dd98802c77242634665
Songyan li <peachlsy@gmail.com>
2019-11-18 00:05:22 +1100

    temp8

--- public/service-worker.js
+++ public/service-worker.js
@@ -113,7 +113,7 @@ self.addEventListener('fetch', function (event) {
 
   event.respondWith(
 
-    caches.open(cacheName).then(function (cache) {
+    caches.open(CACHE_NAME).then(function (cache) {
       return cache.match(event.request).then(function (response) {
         return response || fetch(event.request).then(function (response) {
           cache.put(event.request, response.clone());
