a3925156d8866f3ede067b4f1bf34f1c30073393
Songyan li <peachlsy@gmail.com>
2019-11-18 16:18:46 +1100

    test_1

--- public/service-worker.js
+++ public/service-worker.js
@@ -22,34 +22,34 @@ const CACHE_NAME = 'static-cache-v2';
 const DATA_CACHE_NAME = 'data-cache-v1';
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/secondPage.html',
-  '/thirdPage.html',
-  '/scripts/app.js',
-  '/scripts/install.js',
-  '/scripts/luxon-1.11.4.js',
-  '/styles/inline.css',
-  '/images/resize.png',
-  '/images/add.svg',
-  '/images/clear-day.svg',
-  '/images/clear-night.svg',
-  '/images/cloudy.svg',
-  '/images/fog.svg',
-  '/images/hail.svg',
-  '/images/install.svg',
-  '/images/partly-cloudy-day.svg',
-  '/images/partly-cloudy-night.svg',
-  '/images/rain.svg',
-  '/images/refresh.svg',
-  '/images/sleet.svg',
-  '/images/snow.svg',
-  '/images/thunderstorm.svg',
-  '/images/tornado.svg',
-  '/images/wind.svg',
-  '/images/SplashScreen.png',
-  '/images/signatureScreen.png',
-  '/images/deliveryScreen.png'
+  './',
+  './index.html',
+  './secondPage.html',
+  './thirdPage.html',
+  './scripts/app.js',
+  './scripts/install.js',
+  './scripts/luxon-1.11.4.js',
+  './styles/inline.css',
+  './images/resize.png',
+  './images/add.svg',
+  './images/clear-day.svg',
+  './images/clear-night.svg',
+  './images/cloudy.svg',
+  './images/fog.svg',
+  './images/hail.svg',
+  './images/install.svg',
+  './images/partly-cloudy-day.svg',
+  './images/partly-cloudy-night.svg',
+  './images/rain.svg',
+  './images/refresh.svg',
+  './images/sleet.svg',
+  './images/snow.svg',
+  './images/thunderstorm.svg',
+  './images/tornado.svg',
+  './images/wind.svg',
+  './images/SplashScreen.png',
+  './images/signatureScreen.png',
+  './images/deliveryScreen.png'
 ];
 
 //用于缓存内容
@@ -95,7 +95,7 @@ self.addEventListener('fetch', (evt) => {
       .catch(() => {
         return caches.open(CACHE_NAME)
           .then((cache) => {
-            return cache.match('secondPage.html');
+            return cache.match('./secondPage.html');
           });
       })
   );
